0b0634dcbc3c20ebda8e81fd982e1d8eb0241ad2
Abdullah khormi <Abdullah.khormi@qeema.net>
2025-03-16 01:39:47 +0300

    handle footer

--- src/app/container/default-layout/default-layout.component.ts
+++ src/app/container/default-layout/default-layout.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HaederComponent } from "../../shared/components/haeder/haeder.component";
 import { FooterComponent } from "../../shared/components/footer/footer.component";
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "../../@views/home/home.component";
 
 @Component({
   selector: 'app-default-layout',
   standalone: true,
-  imports: [HaederComponent, FooterComponent],
+  imports: [HaederComponent, FooterComponent, RouterOutlet, HomeComponent],
   templateUrl: './default-layout.component.html',
   styleUrl: './default-layout.component.scss'
 })
