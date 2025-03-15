8a6bff2743ec9dd9aedd4df690c168dcd0204f4e
Abdullah khormi <Abdullah.khormi@qeema.net>
2025-03-16 00:35:04 +0300

    handle header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { DefaultLayoutComponent } from './container/default-layout/default-layout.component';
+import { HomeComponent } from '../app/@views/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    children: [
+      {
+        path: '', // Default child route
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+    ],
+  },
+];
--- src/app/container/default-layout/default-layout.component.ts
+++ src/app/container/default-layout/default-layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HaederComponent } from "../../shared/components/haeder/haeder.component";
+import { FooterComponent } from "../../shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-default-layout',
   standalone: true,
-  imports: [],
+  imports: [HaederComponent, FooterComponent],
   templateUrl: './default-layout.component.html',
   styleUrl: './default-layout.component.scss'
 })
